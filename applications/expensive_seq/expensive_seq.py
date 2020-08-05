6094ec2bffcc9e20ed2d112556bce0585711ce72
Liam Moore <liam.moore.dev@gmail.com>
2020-08-05 19:15:36 +0000

    completes expensive sequence problem

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,23 @@
 # Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
 
+    # set base case for recursion
+    if x <= 0:
+        return y + z
+    
+    # create a unique key for our cache for x,y,z values
+    key = f"x{x}y{y}z{z}"
+
+    # check if the answer to this particular calculation is already memoized in the cache
+    if key not in cache:
+        # if not, then save it to the cache
+        cache[key] = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+
+    # return the cached answer
+    return cache[key]
 
 
 if __name__ == "__main__":
