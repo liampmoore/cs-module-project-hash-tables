4c2c7608a932972a81f353ed430f94043d5216a1
Liam Moore <liam.moore.dev@gmail.com>
2020-08-05 23:44:54 +0000

    completes histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,45 @@
 # Your code here
 
+import sys
+
+def count_occurences(string):
+    table = {}
+    current_word = ''
+    longest_word = ''
+    for i in range(len(string)):
+        if string[i].lower() >= 'a' and string[i].lower() <= 'z' or string[i] == "'":
+            current_word = current_word + string[i].lower()
+        else:
+            if string[i] == "'":
+                pass
+            if current_word != '':
+                if len(current_word) > len(longest_word):
+                    longest_word = current_word
+                if current_word in table:
+                    table[current_word] += 1
+                    current_word = ''
+                else:
+                    table[current_word] = 1
+                    current_word = ''
+
+    return (table, longest_word)
+
+def print_histogram(table, longest):
+    sorted_words = sorted(table.items(), key=lambda word: (-word[1], word[0]) )
+    for word in sorted_words:
+        line = word[0]
+        if len(line) < len(longest):
+            line = line + (' ' * (len(longest) - len(line)))
+        line = line + '  ' + ('#' * word[1])
+        print(line)
+        
+
+input_filename = sys.argv[1]
+
+input_file = open(input_filename, 'r')
+
+input_string = input_file.read()
+
+(table, longest_word) = count_occurences(input_string)
+
+print_histogram(table, longest_word)
